9dcdb57110e468c8bc4f87f3cee43db67eb19ac8
Kaan Taze <bkaantaze@gmail.com>
2020-08-31 12:07:52 +0300

    init project

---
+++ Server/Models/ApplicationUser.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CugemderPortal.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string CustomClaim { get; set; }
+
+        public string firstName { get; set; }
+
+        public string surName { get; set; }
+
+        public int? gender { get; set; }
+
+        public string? photoUrl { get; set; }
+
+        public int? company { get; set; }
+
+        public int? jobTitle { get; set; }
+
+        public int? speciality { get; set; }
+
+        public int? position { get; set; }
+
+        public int? points { get; set; }
+
+        public int? notifications { get; set; }
+
+        public string? referencedBy { get; set; }
+
+        public int? group { get; set; }
+
+        public DateTime? createdAt { get; set; }
+
+        public DateTime? updatedAt { get; set; }
+
+    }
+}
