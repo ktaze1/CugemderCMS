485f9600ab525f210ac2c8d06047b1e41ae1afb9
Kaan Taze <bkaantaze@gmail.com>
2020-09-04 04:21:43 +0300

    init re-do

---
+++ CugemderPortal/Server/Data/Migrations/20200904000040_init2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CugemderPortal.Server.Data.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "speciality",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "speciality",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
